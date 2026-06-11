dc6ae544b0e95eef19bff27a0bbf0d0f1f9c7ab6
lucgus11 <lucas.gustin@indse.be>
2026-06-11 11:01:14 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent",
+    title: "FitFlow",
+  },
+};
